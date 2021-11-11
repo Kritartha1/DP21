8e3a32a048db102ca28b7185bb903f900b9410e0
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2021-11-12 01:35:31 +0530

    Create ClimbingStairsTopDown.java

---
+++ Day2/ClimbingStairsTopDown.java
@@ -0,0 +1,15 @@
+public class ClimbStairs {
+
+    int[] dp=new int[46];
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        else if(dp[n]!=0){
+            return dp[n];
+        }
+        dp[n]=climbStairs(n-1)+climbStairs(n-2);
+        return dp[n];
+
+    }
+}
