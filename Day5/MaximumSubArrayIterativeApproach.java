c4fc097c883c30ced04c1b944be8c0dde0dd489d
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2021-11-21 13:54:23 +0530

    Create MaximumSubArrayIterativeApproach.java

---
+++ Day5/MaximumSubArrayIterativeApproach.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=Integer.MIN_VALUE;
+        int currSum=0;
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            if(currSum<nums[i]){
+                currSum=nums[i];
+            }
+            sum=Math.max(sum,currSum);
+        }
+        return sum;
+        
+    }
+}
