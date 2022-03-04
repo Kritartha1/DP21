a0261610a2c7705775c86a4248a158b1bb8229c9
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-03-04 17:15:36 +0530

    Update decode-ways.java

--- Day10/decode-ways.java
+++ Day10/decode-ways.java
@@ -37,3 +37,56 @@ class Solution {
     }
 }
 //**************************************************************************************************************************************************************************
+//Bottom up:
+class Solution {
+    public int numDecodings(String s) {
+        
+        int n=s.length();
+        int dp[]=new int[n+1];
+        dp[n]=1;
+        
+        for(int i=n-1;i>=0;i--){
+          
+            if(s.charAt(i)!='0'){
+                dp[i]=dp[i+1];
+                if((i<n-1)&&(s.charAt(i)=='1'||s.charAt(i)=='2'&&s.charAt(i+1)<'7')){
+                    dp[i]+=dp[i+2];
+                }
+                
+            }
+        }
+        
+        return dp[0];
+        
+    }
+}
+//***************************************************************************************************************************************************************************
+//Iterative approach: Space efficient:
+class Solution {
+    public int numDecodings(String s) {
+        
+        int n=s.length();
+        
+        int a=1;
+        int b=0;
+        int temp=0;
+        
+        for(int i=n-1;i>=0;i--){
+            
+            temp=0;
+            if(s.charAt(i)!='0'){
+                temp=a;
+                if((i<n-1)&&(s.charAt(i)=='1'||s.charAt(i)=='2'&&s.charAt(i+1)<'7')){
+                    temp+=b;
+                }
+                
+                
+            }
+            b=a;
+            a=temp;
+        }
+        
+        return a;
+        
+    }
+}
