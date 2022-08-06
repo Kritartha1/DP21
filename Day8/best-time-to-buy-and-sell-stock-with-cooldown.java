be390d9b6dae1f0c1eca18e817b4cb92ac485a80
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-08-06 13:59:45 +0530

    Update best-time-to-buy-and-sell-stock-with-cooldown.java

--- Day8/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ Day8/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -77,3 +77,51 @@ class Solution {
         return s0;
     }
 }
+
+//Next approach:
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int min=prices[0];
+        int ans=0;
+        int prevAns=0;
+        for(int i=2;i<=n;++i){
+            int temp=ans;//will be dp[i-2] for next iteration.
+            ans=Math.max(ans,prices[i-1]-min);
+            min=Math.min(prices[i-1]-prevAns,min);
+            prevAns=temp;
+            
+                
+        }
+        return ans;
+    }
+}
+
+//Next approach:
+class Solution {
+    int[] dp;
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,prices);
+    }
+    
+    int solve(int n,int[] prices){
+        if(n<=0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        int sell=prices[n-1];
+        int ans=solve(n-1,prices);
+        for(int j=n-1;j>0;--j){
+            if(prices[j-1]<=sell){
+                ans=Math.max(sell-prices[j-1]+solve(j-2,prices),ans);
+            }
+        }
+        return dp[n]=ans;
+    }
+}
